98e9af1bd4217551da286dc56a67a80862cce73b
Ryan Shin <changyeolshin@gmail.com>
2017-11-18 21:39:04 -0800

    Added 2 more solutions for the reverse string exercise.

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -8,15 +8,37 @@
 // My solution
 function reverse(str) {
   let arr = []
-  for (let i = str.length; i >= 0; i--) {
+  for (let i = str.length - 1; i >= 0; i--) {
     arr.push(str[i])
   }
   let newString = arr.join('')
   return newString
 }
 
+// Sometimes interviewers don't want you to use the reverse function.
 function reverseTwo(str) {
   return str.split('').reverse().join('')
 }
 
-module.exports = reverse
+// Try to avoid for loops
+function reverseThree(str) {
+  let reversed = ''
+  for (let character of str) {
+    reversed = character + reversed
+  }
+  return reversed
+}
+
+// Better way of solving this problem
+function reverseFour(str) {
+  return str
+    .split('')
+    .reduce((reversed, character) => character + reversed, '')
+}
+
+module.exports = {
+  reverse,
+  reverseTwo,
+  reverseThree,
+  reverseFour
+}
--- exercises/reversestring/test.js
+++ exercises/reversestring/test.js
@@ -1,5 +1,4 @@
-const reverse = require('./index')
-const reverseTwo = require('./index')
+const { reverse, reverseTwo, reverseThree, reverseFour } = require('./index')
 
 test('Reverse function exists', () => {
   expect(reverse).toBeDefined()
@@ -13,14 +12,38 @@ test('Reverse reverses a string', () => {
   expect(reverse('    abcd')).toEqual('dcba    ')
 })
 
-test('Reverse function exists', () => {
+test('ReverseTwo function exists', () => {
   expect(reverseTwo).toBeDefined()
 })
 
-test('Reverse reverses a string', () => {
+test('ReverseTwo reverses a string', () => {
   expect(reverseTwo('abcd')).toEqual('dcba')
 })
 
-test('Reverse reverses a string', () => {
+test('ReverseTwo reverses a string', () => {
   expect(reverseTwo('    abcd')).toEqual('dcba    ')
+})
+
+test('ReverseThree function exists', () => {
+  expect(reverseThree).toBeDefined()
+})
+
+test('ReverseThree reverses a string', () => {
+  expect(reverseThree('abcd')).toEqual('dcba')
+})
+
+test('ReverseThree reverses a string', () => {
+  expect(reverseThree('    abcd')).toEqual('dcba    ')
+})
+
+test('ReverseFour function exists', () => {
+  expect(reverseFour).toBeDefined()
+})
+
+test('ReverseFour reverses a string', () => {
+  expect(reverseFour('abcd')).toEqual('dcba')
+})
+
+test('ReverseFour reverses a string', () => {
+  expect(reverseFour('    abcd')).toEqual('dcba    ')
 })
